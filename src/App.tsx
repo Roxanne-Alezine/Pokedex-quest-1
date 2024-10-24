f66298134bfde77fc174888acc1ba18200647c0a
Roxanne <roxanne.c.alezine@gmail.com>
2024-10-24 13:46:10 +0200

    definition of type of props

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import PokemonCard from "./components/PokemonCard";
 
 
 
+
 function App() {
   const pokemonList = [
     {
       name: "bulbasaur",
-      imgSrc:
+      image:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
